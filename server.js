85aa5546144435875c4ae42cefc9c2f66b569e08
Spinozist <ewoodworth@atlantaregional.org>
2019-01-03 13:36:14 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to the Mongo DB
 
